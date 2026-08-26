387346b5822cd7362fd88084105817b8401ed2e0
Amin <minhdinhkhoi@gmail.com>
2026-08-26 09:29:10 +0700

    Turn on key passwords

--- src/js/app.js
+++ src/js/app.js
@@ -31,7 +31,7 @@ import { renderGaussChart, renderIkigaiChart } from './ui/charts.js';
 
 export const CONFIG = {
   SHOW_ENGINE_SWITCHER: false,
-  REQUIRE_PASSWORD: false
+  REQUIRE_PASSWORD: true
 };
 
 let isMathCorrectedEngine = true;
